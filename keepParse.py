5c73463d69eb7d4c4794e82088cbc60c2ce63380
josephlin <josephlin117@gmail.com>
2018-11-27 21:02:46 +0800

    init & add keepParse

---
+++ keepParse.py
@@ -0,0 +1,20 @@
+# -*- coding:utf8 -*-
+import json
+import pandas as pd
+import numpy as np
+
+def json_parse(source_file):
+    
+    with open(source_file, encoding='utf-8') as f:
+        for r in f.readlines():
+            data_dict = json.loads(r)
+        
+    return data_dict
+
+if __name__ == '__main__':    
+
+    data_daily = './data_daily.txt'
+    data_month = './data_month.txt'
+    data_week = './data_week.txt'
+    
+    data_dict = json_parse(data_daily)
